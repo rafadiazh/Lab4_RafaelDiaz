a5d6bab14d46b67d6de9c615d676236bd69a8256
Rafael Diaz Handal <rafadiazh98@gmail.com>
2017-05-12 14:00:40 -0600

    Almacen

--- Lab4_RafaelDiaz/src/lab4_rafaeldiaz/Almacen.java
+++ Lab4_RafaelDiaz/src/lab4_rafaeldiaz/Almacen.java
@@ -13,5 +13,61 @@ import java.util.ArrayList;
  */
 public class Almacen {
     ArrayList<Producto> productos = new ArrayList();
+    ArrayList<Empleado> empleados = new ArrayList();
+    ArrayList<Cliente> clientes = new ArrayList();
     double tamanometros, altura;
+
+    public Almacen() {
+    }
+
+    public Almacen(double tamanometros, double altura) {
+        this.tamanometros = tamanometros;
+        this.altura = altura;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public double getTamanometros() {
+        return tamanometros;
+    }
+
+    public void setTamanometros(double tamanometros) {
+        this.tamanometros = tamanometros;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Almacen{" + "productos=" + productos + ", empleados=" + empleados + ", clientes=" + clientes + ", tamanometros=" + tamanometros + ", altura=" + altura + '}';
+    }
+    
 }
